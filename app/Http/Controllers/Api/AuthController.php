94ae6c9eb34b137322fc5daacd7acd96a4fc4156
Mário Lucas <mariolucasdev@gmail.com>
2024-05-08 02:50:55 -0300

    adding register user api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ApiLoginRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -32,6 +33,32 @@ class AuthController extends Controller
                 'token' => $token,
                 'user' => $user
             ]
+        );
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->successResponse(
+            message: 'User registered',
+            data: [
+                'token' => $token,
+                'user' => $user
+            ],
+            code: 201
         );
     }
 }
--- tests/Feature/Api/Auth/RegisterTest.php
+++ tests/Feature/Api/Auth/RegisterTest.php
@@ -1,7 +1,93 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+test('register user api', function () {
+    $user = [
+        'name' => 'Mário Lucas',
+        'email' => 'mario@test.com',
+        'password' => 'password',
+        'password_confirmation' => 'password'
+    ];
 
-    $response->assertStatus(200);
-});
+    $response = $this->postJson('/api/auth/register', $user);
+
+    $response
+        ->assertStatus(201)
+        ->assertHeader('content-type', 'application/json')
+        ->assertJsonStructure([
+            'message',
+            'status',
+            'data' => [
+                'token',
+                'user' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at'
+                ]
+            ]
+        ]);
+})->group('auth');
+
+test('register user api without confirm password', function () {
+    $user = [
+        'name' => 'Mário Lucas',
+        'email' => 'mario@test.com',
+        'password' => 'password',
+    ];
+
+    $response = $this->postJson('/api/auth/register', $user);
+
+    $response
+        ->assertStatus(422)
+        ->assertHeader('content-type', 'application/json')
+        ->assertJsonStructure([
+            'message',
+            'errors' => [
+                'password'
+            ]
+        ]);
+})->group('auth');
+
+test('register user api without name and email', function () {
+    $user = [
+        'password' => 'password',
+        'password_confirmation' => 'password'
+    ];
+
+    $response = $this->postJson('/api/auth/register', $user);
+
+    $response
+        ->assertStatus(422)
+        ->assertHeader('content-type', 'application/json')
+        ->assertJsonStructure([
+            'message',
+            'errors' => [
+                'name',
+                'email'
+            ]
+        ]);
+})->group('auth');
+
+test('register user with exists email', function () {
+    $user = [
+        'name' => 'Mário Lucas',
+        'email' => 'mario@test.com',
+        'password' => 'password',
+        'password_confirmation' => 'password'
+    ];
+
+    $this->postJson('/api/auth/register', $user);
+
+    $response = $this->postJson('/api/auth/register', $user);
+
+    $response
+        ->assertStatus(422)
+        ->assertHeader('content-type', 'application/json')
+        ->assertJsonStructure([
+            'message',
+            'errors' => [
+                'email'
+            ]
+        ]);
+})->group('auth');
